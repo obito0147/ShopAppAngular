83edfe37e23c1c2604c4e5908bfd9f1d792c97f3
Tuan Nguyen <ravennguyen113@gmail.com>
2024-11-27 19:01:56 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
