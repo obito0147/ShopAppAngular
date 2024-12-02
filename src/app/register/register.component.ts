17f6f8382089b4818befebd664a16e32444bfb15
Tuan Nguyen <ravennguyen113@gmail.com>
2024-12-02 23:21:41 +0700

    validate

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [FooterComponent, HeaderComponent,FormsModule],
+  imports: [FooterComponent, HeaderComponent,CommonModule,FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm! : NgForm;
   //khai báo các biến tương ứng với các trường dữ liệu trong form
   phone: string;
   password: string;
@@ -41,5 +44,33 @@ export class RegisterComponent {
                     `isAccepted: ${this.isAccepted}`+
                     `dateOfBirth: ${this.dateOfBirth}`
     alert(message);
+  }
+  //how to chekc password match ?
+  checkPasswordMatch(){
+    if(this.password !== this.retypePassword){
+      this.registerForm.form.controls['retypePassword'].setErrors({'passwordMismatch': true});
+    }else{
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+  checkAge(){
+    if(this.dateOfBirth){
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if(monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())){
+        age--;
+      }
+      if(age < 18){
+        this.registerForm.form.controls['dateOfBirth'].setErrors({'invalidAge':true});
+      }else{
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
+    // <p class="text-danger"
+    //                         *ngIf="registerForm.form.controls['dateOfBirth']?.hasError('invalidAge') && registerForm.form.touched">
+    //                         Bạn chưa đủ 18 tuổi
+    //                     </p>
   }
 }
